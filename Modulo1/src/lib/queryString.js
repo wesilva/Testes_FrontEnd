1070997733b366e266c4ca007b191608ed22b3fb
Wellington Silva <wellingtonsilva@digix.com.br>
2021-05-11 08:31:48 -0400

    Implementando método para fazer parse da Query string

--- Modulo1/src/lib/queryString.js
+++ Modulo1/src/lib/queryString.js
@@ -7,3 +7,15 @@ const keyValueToString = ([key, value]) => {
 
 module.exports.queryString = obj =>
   Object.entries(obj).map(keyValueToString).join('&');
+
+module.exports.parse = string =>
+  Object.fromEntries(
+    string.split('&').map(item => {
+      let [key, value] = item.split('=');
+
+      if (value.indexOf(',') > -1) {
+        value = value.split(',');
+      }
+      return [key, value];
+    }),
+  );
--- Modulo1/src/lib/queryString.spec.js
+++ Modulo1/src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
@@ -32,5 +32,33 @@ describe('Object to query string', () => {
     expect(() => {
       queryString(obj);
     }).toThrowError();
+  });
+});
+
+describe('Query string to object', () => {
+  it('should convert a query string to object', () => {
+    const qs = 'name=Wellington&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Wellington',
+      profession: 'developer',
+    });
+  });
+
+  it('should convert a query string of single key-value pair to object', () => {
+    const qs = 'name=Wellington';
+
+    expect(parse(qs)).toEqual({
+      name: 'Wellington',
+    });
+  });
+
+  it('should convert a query string to an object taking care of comma separated values', () => {
+    const qs = 'name=Wellington&abilities=JS,TDD';
+
+    expect(parse(qs)).toEqual({
+      name: 'Wellington',
+      abilities: ['JS', 'TDD'],
+    });
   });
 });
