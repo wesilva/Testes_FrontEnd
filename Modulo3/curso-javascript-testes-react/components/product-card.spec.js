631e3d9b1a1c3c40a82db7a0edacf200d12c228d
Wellington Silva <wellingtonsilva@digix.com.br>
2021-05-26 07:29:50 -0400

    Criando testes em componentes react

---
+++ Modulo3/curso-javascript-testes-react/components/product-card.spec.js
@@ -0,0 +1,48 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import ProductCard from './product-card';
+
+const product = {
+  title: 'Relógio bonito',
+  price: '22.00',
+  image: 'photo',
+};
+
+const addToCart = jest.fn();
+
+const renderProductCard = () => {
+  render(<ProductCard product={product} addToCart={addToCart} />);
+};
+
+describe('ProductCard', () => {
+  it('should render ProductCard', () => {
+    renderProductCard();
+
+    expect(screen.getByTestId('product-card')).toBeInTheDocument();
+  });
+
+  it('should display proper content', () => {
+    renderProductCard();
+
+    expect(
+      screen.getByText(new RegExp(product.title, 'i')),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(new RegExp(product.price, 'i')),
+    ).toBeInTheDocument();
+    expect(screen.getByTestId('image')).toHaveStyle({
+      backgroundImage: product.image,
+    });
+  });
+
+  it('should call props.addToCart() when button gets clicked', async () => {
+    renderProductCard();
+
+    const button = screen.getByRole('button');
+    //screen.debug(button);
+
+    await fireEvent.click(button);
+
+    expect(addToCart).toHaveBeenCalledTimes(1);
+    expect(addToCart).toHaveBeenCalledWith(product);
+  });
+});
