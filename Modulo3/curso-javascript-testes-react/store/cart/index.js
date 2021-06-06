4667749b0b7be469519dd5d2c26b1ca8575ae23c
Wellington Silva <wellingtonsilva@digix.com.br>
2021-06-06 09:56:20 -0400

    Adicionando metodos para remover itens do cart

--- Modulo3/curso-javascript-testes-react/store/cart/index.js
+++ Modulo3/curso-javascript-testes-react/store/cart/index.js
@@ -19,16 +19,32 @@ export const useCartStore = create((set) => {
           state.open = !state.open;
         });
       },
-      reset() {
-        setState((store) => {
-          store.state = initialState;
-        });
-      },
       add(product) {
         setState(({ state }) => {
           if (!state.products.includes(product)) {
             state.products.push(product);
           }
+        });
+      },
+      remove(product) {
+        setState(({ state }) => {
+          const exists = !!state.products.find(({ id }) => id === product.id);
+
+          if (exists) {
+            state.products = state.products.filter(({ id }) => {
+              return id !== product.id;
+            });
+          }
+        });
+      },
+      removeAll() {
+        setState(({ state }) => {
+          state.products = [];
+        });
+      },
+      reset() {
+        setState((store) => {
+          store.state = initialState;
         });
       },
     },
